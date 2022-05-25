21e834720d91347ebd8b014e703f8aeafae3d61e
OPALWAYS <103804878+OPALWAYS@users.noreply.github.com>
2022-05-25 20:02:29 +0530

    Update ban.py

--- ban.py
+++ ban.py
@@ -122,10 +122,10 @@ async def banall(event):
                if user.id not in admins_id:
                     await event.client(EditBannedRequest(event.chat_id, user.id, RIGHTS))
                     bann += 1
-                    await asyncio.sleep(0.05)
+                    await asyncio.sleep(0.01)
              except Exception as e:
                    print(str(e))
-                   await asyncio.sleep(0.05)
+                   await asyncio.sleep(0.01)
          await RiZoeL.edit(f"**Users Banned Successfully ! \n\n Banned Users:** `{bann}` \n **Total Users:** `{all}`")
 
     
